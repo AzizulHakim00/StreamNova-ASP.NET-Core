5f30dc196baaa66979dc292b675f2a5d8150e7af
Azizul Hakim Omor <157488702+AzizulHakim00@users.noreply.github.com>
2026-07-22 08:19:41 +0600

    Add previous and next titles to the player

--- ViewModels/WatchViewModel.cs
+++ ViewModels/WatchViewModel.cs
@@ -5,6 +5,8 @@ namespace StreamNova.ViewModels;
 public sealed class WatchViewModel
 {
     public Movie Movie { get; set; } = new();
+    public Movie? PreviousMovie { get; set; }
+    public Movie? NextMovie { get; set; }
     public int ResumeSeconds { get; set; }
     public string PlaybackKind { get; set; } = "none";
     public string? PlaybackUrl { get; set; }
